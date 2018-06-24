7d1def3462a2afe01c3cac89dfa9f321c0ac7868
Jedai Saboteur <dev@paradoxinversion.com>
2018-06-24 15:48:22 -0700

    Replace react implementation

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -8,17 +8,16 @@ import TweetSearch from "../../components/TweetSearch/TweetSearch";
 import axios from "axios";
 
 class MainPage extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      data: null,
-
+      data: null
     };
     this.searchUser = this.searchUser.bind(this);
     this.renderHashTags = this.renderHashTags.bind(this);
   }
 
-  async searchUser(username, tweetsToGet)  {
+  async searchUser(username, tweetsToGet) {
     const config = {
       withCredentials: true,
       params: {
@@ -30,39 +29,50 @@ class MainPage extends Component {
       }
     };
 
-    try{
-      const result = await axios.get(`/search`, config);
+    try {
+      const result = await axios.get(`http://localhost:3001/search`, config);
       this.setState({
         data: result.data
       });
-    } catch (e){
+    } catch (e) {
       throw e;
     }
   }
 
-  renderHashTags(){
-    if (this.state.data){
-      return <HashtagList heading="Most used Hashtags" id="most-used-hashtags" hashtags={this.state.data.mostCommonHashtags} />;
+  renderHashTags() {
+    if (this.state.data) {
+      return (
+        <HashtagList
+          heading="Most used Hashtags"
+          id="most-used-hashtags"
+          hashtags={this.state.data.mostCommonHashtags}
+        />
+      );
     } else {
       return null;
     }
   }
 
-  renderIntro(){
-    if (!this.props.isAuthenticated){
+  renderIntro() {
+    if (!this.props.isAuthenticated) {
       return (
         <div className="showcase">
           <h1 className="showcase-title">Overview</h1>
           <p className="showcase-body">Welcome to Overview!</p>
-          <p>Overview helps you get a quick snapshot of a twitter user, by making it easier to view their tweets in mass.
-            Sign in with Twitter to get started.</p>
+          <p>
+            Overview helps you get a quick snapshot of a twitter user, by making
+            it easier to view their tweets in mass. Sign in with Twitter to get
+            started.
+          </p>
         </div>
       );
-    } else{
-      if (!this.state.data){
+    } else {
+      if (!this.state.data) {
         return (
           <div className="showcase">
-            <p className="showcase-body">Search for a twitter user by their @screenname to get started.</p>
+            <p className="showcase-body">
+              Search for a twitter user by their @screenname to get started.
+            </p>
           </div>
         );
       }
@@ -70,56 +80,58 @@ class MainPage extends Component {
     }
   }
 
-  renderUserProfile(){
-    if (this.state.data){
-      return (
-        <UserProfile
-          user={this.state.data.user} />
-      );
+  renderUserProfile() {
+    if (this.state.data) {
+      return <UserProfile user={this.state.data.user} />;
     } else {
       return null;
     }
   }
 
-  renderTweetList(tweets, heading, id){
-    return (
-      <TweetList
-        heading={heading}
-        id={id}
-        tweets={tweets} />
-    )
+  renderTweetList(tweets, heading, id) {
+    return <TweetList heading={heading} id={id} tweets={tweets} />;
   }
 
-  renderTweetDisplay(){
+  renderTweetDisplay() {
     let tweetLists;
-    if (this.state.data){
+    if (this.state.data) {
       tweetLists = [
-        this.renderTweetList(this.state.data.processedTweets, "Latest", "latest-tweets"),
-        this.renderTweetList(this.state.data.topTweetsByRT, "Top Tweets", "top-tweets")
+        this.renderTweetList(
+          this.state.data.processedTweets,
+          "Latest",
+          "latest-tweets"
+        ),
+        this.renderTweetList(
+          this.state.data.topTweetsByRT,
+          "Top Tweets",
+          "top-tweets"
+        )
       ];
     }
 
-    if (this.state.data){
-      return(
+    if (this.state.data) {
+      return (
         <div className="tweet-display ">
           <div className="tab-group">
             <TweetSearch
               tweets={this.state.data.processedTweets}
-              user={this.state.data.user} />
+              user={this.state.data.user}
+            />
             <TweetList
               heading="Latest"
               id="latest-tweets"
               tweets={this.state.data.processedTweets}
-              user={this.state.data.user} />
+              user={this.state.data.user}
+            />
             <TweetList
               heading="Top Tweets"
               id="top-tweets"
               tweets={this.state.data.topTweetsByRT}
-              user={this.state.data.user} />
+              user={this.state.data.user}
+            />
             {this.renderHashTags()}
           </div>
           {/* <TabGroup tabs={tweetLists} /> */}
-
         </div>
       );
     } else {
@@ -127,21 +139,20 @@ class MainPage extends Component {
     }
   }
 
-  renderTwitterData(){
-    if (this.props.isAuthenticated){
+  renderTwitterData() {
+    if (this.props.isAuthenticated) {
       return (
         <div>
           {this.renderUserProfile()}
           {this.renderTweetDisplay()}
         </div>
-      )
-
+      );
     } else {
       return null;
     }
   }
 
-  renderMainPage(){
+  renderMainPage() {
     return (
       <div className="content-wrapper">
         {this.renderIntro()}
@@ -155,9 +166,7 @@ class MainPage extends Component {
   render() {
     return (
       <div className="main-page">
-        <Header
-          {...this.props}
-          search={this.searchUser}/>
+        <Header {...this.props} search={this.searchUser} />
         {this.renderMainPage()}
       </div>
     );
